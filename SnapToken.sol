f60323f4555f85392198103dcfd0c838c032490d
pbarstad <31356410+pbarstad@users.noreply.github.com>
2019-04-29 19:34:33 -0600

    Runs...
    burnToken() and updatePrice() both cause the gas limit errors... still not sure on how to fix those, or send ether yet. gonna look at it in class tomorrow

--- SnapToken.sol
+++ SnapToken.sol
@@ -18,14 +18,14 @@ contract SnapToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, usingO
     uint8 public constant DECIMALS = 18;
     uint256 public constant INITIAL_SUPPLY = 0 * (10 ** uint256(DECIMALS));
     
-    mapping (string => address) public vendors; //keep list of vendors
+    mapping (string => address payable) public vendors; //keep list of vendors
     mapping (string => address) public members; //keep list of SNAP beneficiaries
     mapping (address => uint256) public allowances; //keep list of allowances (only members)
-    address owner;
-    string[] memberNames;
-    string[] vendorNames;
+    address payable public owner;
+    string[] public memberNames;
+    string[] public vendorNames;
     
-    uint256 etherPrice;
+    uint256 public etherPrice;
     
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
@@ -33,6 +33,12 @@ contract SnapToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, usingO
     constructor () public ERC20Detailed("SnapToken", "SNAP", DECIMALS) {
         _mint(msg.sender, INITIAL_SUPPLY);
         owner = msg.sender;
+        updatePrice(); //fill ether price when contract is deployed
+        
+        //remove after testing
+        addMember("Paul", 0x2d03e08DEf83B3440F7AACA1706308D2D7B550b8, 500);
+        addVendor("King Soopers", 0x19edA1943E6835Cd4723E1d350096E87D538ab2b);
+        giveAllowances();
     }
     
     //mint function for admin
@@ -60,15 +66,19 @@ contract SnapToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, usingO
     }
     
     //vendor transfer to us
-    function exchangeSnap(string memory me, uint256 amount) public returns (bool) {
+    function exchangeSnap(string memory me, uint256 amount) public returns (uint256) {
         require(vendors[me] == msg.sender); //only vendors can use this function
         require(balanceOf(msg.sender) >= amount); //gas?
         _transfer(msg.sender, owner, amount); //safeTransfer amount to owner
-        _burnFrom(owner, amount);
+        //_burnFrom(owner, amount); //causes gas error
         //oracle for current value of ether
-        updatePrice();
-        uint256 etherAmount = amount/etherPrice;
+        //updatePrice(); //causes gas error
+        uint256 etherAmount = amount/etherPrice; //problem of sending old amount?
+        return etherAmount;
         //transfer etherAmount from owner to msg.sender
+        
+        //require() owner has enough ether
+        //msg.sender.transfer(etherAmount); 
     }
         
     //get balance functions for members and vendors
@@ -76,7 +86,7 @@ contract SnapToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, usingO
         return balanceOf(msg.sender);
     }
     
-    //get etherPRice
+    //get etherPrice
     function getEtherPrice() public view returns(uint256) {
         return etherPrice;
     }
@@ -84,13 +94,15 @@ contract SnapToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, usingO
     //add users/vendors
     function addMember(string memory newName, address newAddress, uint256 newAllowance) public {
         require(owner == msg.sender); //only owner can add new SNAP members
+        require(members[newName] == address(0)); //only unique names allowed
         memberNames.push(newName);
         members[newName] = newAddress;
         allowances[newAddress] = newAllowance;
     }
     
-    function addVendor(string memory newVendor, address newAddress) public {
+    function addVendor(string memory newVendor, address payable newAddress) public {
         require(owner == msg.sender); //only owner can add new vendors
+        require(vendors[newVendor] == address(0)); //only unique names allowed
         vendorNames.push(newVendor);
         vendors[newVendor] = newAddress;
     }
@@ -105,7 +117,10 @@ contract SnapToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, usingO
         etherPrice = parseInt(result);
     }
     
-    //check if in contract/login
+    /*function ownerAddEther(uint256 etherIn) public payable returns(uint256) {
+        require(msg.sender == owner);
+        
+    }*/
     
 }
 
