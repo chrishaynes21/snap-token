856b9327ffd44ecf57fabfad836f11f3a4f7e1d4
pbarstad <31356410+pbarstad@users.noreply.github.com>
2019-05-01 14:42:20 -0600

    SnapToken.sol complete!

--- SnapToken.sol
+++ SnapToken.sol
@@ -18,14 +18,15 @@ contract SnapToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, usingO
     uint8 public constant DECIMALS = 18;
     uint256 public constant INITIAL_SUPPLY = 0 * (10 ** uint256(DECIMALS));
     
-    mapping (string => address payable) public vendors; //keep list of vendors
+    mapping (string => address) public vendors; //keep list of vendors
     mapping (string => address) public members; //keep list of SNAP beneficiaries
     mapping (address => uint256) public allowances; //keep list of allowances (only members)
-    address payable public owner;
+    address public owner;
     string[] public memberNames;
     string[] public vendorNames;
     
-    uint256 public etherPrice;
+    uint256 public etherPrice = 150;
+    uint256 public transferAmount;
     
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
@@ -36,24 +37,23 @@ contract SnapToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, usingO
         updatePrice(); //fill ether price when contract is deployed
         
         //remove after testing
-        addMember("Paul", 0x2d03e08DEf83B3440F7AACA1706308D2D7B550b8, 500);
-        addVendor("King Soopers", 0x19edA1943E6835Cd4723E1d350096E87D538ab2b);
-        giveAllowances();
+        //addMember("Paul", 0x2d03e08DEf83B3440F7AACA1706308D2D7B550b8, 100);
+        //addVendor("King Soopers", 0x19edA1943E6835Cd4723E1d350096E87D538ab2b);
     }
     
-    //mint function for admin
-    function mint(address to, uint256 value) public returns (bool) {
-        require(msg.sender == owner);
-        _mint(to, value);
-        return true;
-    }
+    
     //transfer allowances monthly
-    function giveAllowances() public returns (bool) {
+    function giveAllowances() payable public returns (bool) {
         require(msg.sender == owner);
+        uint256 sum = 0;
         for(uint i = 0; i < memberNames.length; i++) { //loop through members
             address tempMemberAddress = members[memberNames[i]];
             _mint(tempMemberAddress, allowances[tempMemberAddress]);
+            sum += allowances[tempMemberAddress];
         }
+        uint256 requiredEther = sum/etherPrice;
+        //owner add allowances/etherPrice to contact balance
+        require(msg.value >= requiredEther); //automatically sends ether to contract
         return true;
     }
     
@@ -61,24 +61,24 @@ contract SnapToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, usingO
     function useSnap(string memory me, string memory vendorName, uint256 amount) public returns (bool) {
         require(vendors[vendorName] != address(0)); //require vendor inside vendors mapping
         require(members[me] == msg.sender); //only members can use this function
-        require(balanceOf(msg.sender) >= amount); //need to worry about gas?
+        require(balanceOf(msg.sender) >= amount); 
         _transfer(msg.sender, vendors[vendorName], amount); //member to vendor transfer
     }
     
     //vendor transfer to us
     function exchangeSnap(string memory me, uint256 amount) public returns (uint256) {
         require(vendors[me] == msg.sender); //only vendors can use this function
-        require(balanceOf(msg.sender) >= amount); //gas?
-        _transfer(msg.sender, owner, amount); //safeTransfer amount to owner
-        //_burnFrom(owner, amount); //causes gas error
-        //oracle for current value of ether
-        //updatePrice(); //causes gas error
-        uint256 etherAmount = amount/etherPrice; //problem of sending old amount?
-        return etherAmount;
-        //transfer etherAmount from owner to msg.sender
+        require(balanceOf(msg.sender) >= amount);
+        updatePrice(); 
+        uint256 etherAmount = (amount * 10**18)/etherPrice; //problem of sending old amount?
+        transferAmount = etherAmount;
         
-        //require() owner has enough ether
-        //msg.sender.transfer(etherAmount); 
+        //no require contract balance as getAllowances forces contract to have
+        //ether backing up snapTokens
+        msg.sender.transfer(transferAmount);
+        _burn(msg.sender, amount); //burn from contract not owner
+            
+        return etherAmount;
     }
         
     //get balance functions for members and vendors
@@ -86,12 +86,20 @@ contract SnapToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, usingO
         return balanceOf(msg.sender);
     }
     
-    //get etherPrice
+    //getters
     function getEtherPrice() public view returns(uint256) {
         return etherPrice;
     }
     
-    //add users/vendors
+    function getTransferAmount() public view returns(uint256) {
+        return transferAmount;
+    }
+    
+    function getContractBalance() public view returns(uint256) {
+        return address(this).balance;
+    }
+    
+    //add users
     function addMember(string memory newName, address newAddress, uint256 newAllowance) public {
         require(owner == msg.sender); //only owner can add new SNAP members
         require(members[newName] == address(0)); //only unique names allowed
@@ -100,16 +108,17 @@ contract SnapToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, usingO
         allowances[newAddress] = newAllowance;
     }
     
-    function addVendor(string memory newVendor, address payable newAddress) public {
+    function addVendor(string memory newVendor, address newAddress) public {
         require(owner == msg.sender); //only owner can add new vendors
         require(vendors[newVendor] == address(0)); //only unique names allowed
         vendorNames.push(newVendor);
         vendors[newVendor] = newAddress;
     }
     
+    //get current ether value via oraclize
     function updatePrice() public returns(uint256) {
-       bytes32 queryID = oraclize_query("URL", "json(https://api.pro.coinbase.com/products/ETH-USD/ticker).price");
-   }
+        bytes32 queryID = oraclize_query("URL", "json(https://api.pro.coinbase.com/products/ETH-USD/ticker).price");
+    }
    
    function __callback(bytes32 myid, string memory result) public {
         require(msg.sender == oraclize_cbAddress(), "msg.sender is not Oraclize");
@@ -117,10 +126,6 @@ contract SnapToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, usingO
         etherPrice = parseInt(result);
     }
     
-    /*function ownerAddEther(uint256 etherIn) public payable returns(uint256) {
-        require(msg.sender == owner);
-        
-    }*/
     
 }
 
